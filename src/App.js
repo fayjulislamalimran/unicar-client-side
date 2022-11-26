1a3033d51671e42fc69f2ab60c03d8d74b865ac5
Fayjul Islam Al Imran <fayjulislamalimran7@gmail.com>
2022-11-26 19:18:29 +0600

    Home page fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import React from "react";
 
 function App() {
   return (
-    <div className="max-w-[1440px]   ">
+    <div className="max-w-[1440px] mr-16 ml-16  ">
       <RouterProvider router={router}></RouterProvider>
     </div>
   );
